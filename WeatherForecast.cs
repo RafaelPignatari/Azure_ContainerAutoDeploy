3a0b681f4f6b7e2152365a0fdd053fa0948d88a5
Rafael Pignatari <rafaelpignatarifantinati@hotmail.com>
2025-02-01 13:40:36 -0300

    Adicionando temperatura Kelvin

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -8,6 +8,8 @@ namespace Azure_ContainerAutoDeploy
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        public int TemperatureK => TemperatureC + 273;
+
         public string? Summary { get; set; }
     }
 }
